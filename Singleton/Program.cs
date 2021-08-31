05ecc0cf1c6afa3c8245431098cb89f8cb58fdb0
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-08-31 16:01:06 +0200

    Adding Example of Singleton Desgin Pattern.

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Demo Code
+            SingleObjectClass singleObject = SingleObjectClass.getInstance();
+            singleObject.ShowMessage();
+
+            #endregion
+            Console.WriteLine("Done!");
+        }
+    }
+}
